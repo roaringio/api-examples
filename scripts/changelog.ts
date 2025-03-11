9d14b17233acd54231195ec815929a0726d72eab
Helgi Kristjansson <helgi@roaring.io>
2025-03-11 16:38:45 +0100

    Changelog

---
+++ scripts/changelog.ts
@@ -0,0 +1,50 @@
+
+async function fetchChangeLog() {
+  const response = await fetch("https://backend.roaring.io/changelogs?type=API&type=DEFAULT&type=WEB", {
+    headers: {
+      "Accept": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const res = await response.json();
+  const changeLog = res.properties.elements;
+  return changeLog;
+}
+
+type Item = {
+  date: string;
+  title: string;
+  description: string;
+  link?: string;
+}
+
+function toMarkdown(data: any) {
+  const items: Item[] = data.map((item: any) => {
+    return {
+      date: item.properties.changeLogAt,
+      title: item.properties.details.title,
+      description: item.properties.details.description,
+      link: item.properties.details.linkUrl,
+    };
+  });
+
+  const markdown = items.map((item) => {
+    let entry = `## ${item.title}\n\n### ${item.date}\n\n${item.description}\n\n`;
+    if (item.link) {
+      const link = item.link.split("/").pop();
+      entry += `[API Docs](https://app.roaring.io/v2/developer/apis/${link})\n\n`;
+    }
+    return entry;
+  });
+  const changelog = `# Change Log\n\n${markdown.join("\n")}`;
+
+  return changelog;
+}
+
+fetchChangeLog().then((data) => {
+  console.log(
+    toMarkdown(data)
+  );
+});
